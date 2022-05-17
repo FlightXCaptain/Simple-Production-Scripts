754ee79a6b41d2fbf3217ffbafd4ffe46c54ea95
Indigo Solutions <84762143+FlightXCaptain@users.noreply.github.com>
2022-05-17 10:56:58 +0800

    Create Change Calendar Visability for other people.ps1

---
+++ Change Calendar Visability for other people.ps1
@@ -0,0 +1,12 @@
+﻿#For Local Exchange
+#$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential Get-Credential -Authentication Basic -AllowRedirection
+#Import-PSSession $Session
+
+#For ExchangeOnline https://docs.microsoft.com/en-us/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
+#Import-Module ExchangeOnlineManagement
+#Connect-ExchangeOnline
+
+Set-MailboxFolderPermission -Identity ExampleMailbox:\calendar -User default -AccessRights LimitedDetails
+
+#Use this instead to allow everyone to see Subject aswell
+#Set-CalendarProcessing -Identity Meetingroom -AddOrganizerToSubject $true -DeleteComments $false -DeleteSubject $false
