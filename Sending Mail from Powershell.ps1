6160342c082c62caccb25e09de50745c79a19d40
Indigo Solutions <84762143+FlightXCaptain@users.noreply.github.com>
2022-05-17 11:09:31 +0800

    New Scripts for Public Repo

---
+++ Sending Mail from Powershell.ps1
@@ -0,0 +1,9 @@
+﻿$Password = 'EnterPasswordHere'
+$name = 'Enter From Email Here'
+$Subject = 'Subject Line'
+$Body = 'Enter body of email here'
+$To = 'Sending to Email Address'
+$pass = $Password | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($name,$pass)
+Send-MailMessage -To $To -SmtpServer smtp.office365.com -Credential $cred -From $name -Subject $Subject -UseSsl -Body $Body
+#there is a variation to send as someone else but you need to have SendAs permissions on their mailbox then just change the -From Variable.
---
+++ Setting Auto Reply on Shared Mailboxes.ps1
@@ -0,0 +1,12 @@
+﻿#To set Shared mailbox OOF (Useful for holidays) Can also be used to set for multiple mailboxes.
+
+Set-MailboxAutoReplyConfiguration -Identity 'Sharedmailbox@example.com' -AutoReplyState Scheduled -StartTime "12/25/2020 00:00:00" -EndTime "1/4/2021 00:00:01" -ExternalMessage 'Thank you for your email.
+We are currently out of the office and will not return until 04/01/21. I assure you that all your emails will be answered as soon as we return.
+Kind Regards,
+'-InternalMessage 'Thank you for your email.
+We are currently out of the office and will not return until 04/01/21. I assure you that all your emails will be answered as soon as we return.
+Kind Regards,
+'
+
+#To Check Existing.
+#Get-MailboxAutoReplyConfiguration -Identity ''
